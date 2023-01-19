3dd0f6fa35bf1de86b440ea66b23a8a9981d9823
Lillian Grassin-Drake <lilgrassin@gmail.com>
2023-01-18 22:54:42 -0800

    added mod-morph macro and caps-word combo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,7 +6,7 @@
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(RSHFT, N),    U_MT(RCTRL, E),    U_MT(LALT, I),     U_MT(RGUI, O),     \
-&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &comma_lpar_c,     &dot_rpar_c,       &kp MINUS,         \
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &comma_lpar,       &dot_rpar,         &kp MINUS,         \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
